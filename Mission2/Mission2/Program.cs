852e8a1b1953fb6c04f2677af0c351b1091b5100
uboston <uboston@byu.edu>
2025-01-14 09:53:38 -0700

    Comments

--- Mission2/Mission2/Program.cs
+++ Mission2/Mission2/Program.cs
@@ -6,8 +6,13 @@ public class Program
 {
     private static void Main(string[] args)
     {
+        // Welcoming to the App
         Console.WriteLine("Welcome to the dice throwing simulator!\n");
+        
+        // Create Instance of Class
         Rolls rolls = new Rolls();
+        
+        // Catch Input
         int RollNum;
         while (true)
         {
@@ -22,14 +27,19 @@ public class Program
                 Console.WriteLine("Invalid input. Try again.");
             }
         }
+        // Make the Rolls
         int[] rollTotals = rolls.SimulateRolls(RollNum);
         
+        // Present the Results
         Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
         Console.WriteLine("Each \'*\' represents 1% of the total number of rolls.");
         Console.WriteLine("Total number of rolls = " + RollNum + ".");
         
+        
+        // For Loop to print the roll combination frequency
         for (int j = 2; j < 13; j++)
         {
+            // Calculate the average for each roll combination 
             Console.Write($"\n{j}: ");
             for (int i = 0; i < (int)Math.Round(((double)rollTotals.Count(x => x == j) / RollNum * 100)); i++)
             {
--- Mission2/Mission2/Rolls.cs
+++ Mission2/Mission2/Rolls.cs
@@ -4,16 +4,22 @@ public class Rolls
 {
     public int[] SimulateRolls(int rollNum)
     {
+        // Import random
         Random rnd = new Random();
+        
+        // Initiate list
         List<int> totals = new List<int>();
         
+        // Roll based on rollNum parameter
         for (int i = 0; i < rollNum; i++)
         {
+            // Roll both dice and sum
             int dice1 = rnd.Next(1, 7);
             int dice2 = rnd.Next(1, 7);
             int total = dice1 + dice2;
             totals.Add(total);
         }
+        // Return the array
         return totals.ToArray();
     }
 }
