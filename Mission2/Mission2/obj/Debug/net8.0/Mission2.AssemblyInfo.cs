0aa532e64f6347da5101b510fc7e886fc7c95915
uboston <uboston@byu.edu>
2025-02-20 09:09:28 -0700

    First Commit

--- Mission2/Mission2/obj/Debug/net8.0/Mission2.AssemblyInfo.cs
+++ Mission2/Mission2/obj/Debug/net8.0/Mission2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Mission2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca0d41b2e63ba5fb878269bd651f69f5636b8aaa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Mission2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Mission2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
